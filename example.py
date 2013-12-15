3e924916f7c9879b2682fe882439dd4d389e3c7b
Nate <gnatnath98@hotmail.com>
2013-12-15 01:39:14 -0800

    Added comments
    Added a few comments for the logically challenged

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from CDL.main import *
 
-IDSInstance = IDScraper('wg')
-TDInstance = ThreadDownloader('wg')
-TDInstance.DL(IDSInstance.ids(pages=1,pless=20))
+IDSInstance = IDScraper('wg')                       #Create an instance of the id scraper on the board 'wg'
+TDInstance = ThreadDownloader('wg')                 #Create an instance of the thread downloader on the board 'wg'
+TDInstance.DL(IDSInstance.ids(pages=1,pless=20))    #Scrape 'wg' for one page of ids in which the threads have less than 20 pictures and download them
